0db57ca310969cc9cb8793f75809e68dd1eb1439
Felipe Gusmao <fgunix@gmail.com>
2021-07-10 15:48:13 -0300

    Update install.sh

--- utils/installer/install.sh
+++ utils/installer/install.sh
@@ -100,7 +100,7 @@ installpacker() {
 
 cloneconfig() {
     echo "Cloning LunarVim configuration"
-    git clone --branch master https://github.com/pingu1m/LunarVim.git ~/.config/nvim
+    git clone --branch rolling https://github.com/pingu1m/LunarVim.git ~/.config/nvim
     cp $HOME/.config/nvim/utils/installer/lv-config.example-no-ts.lua $HOME/.config/nvim/lv-config.lua
     nvim --headless \
         +'autocmd User PackerComplete sleep 100m | qall' \
